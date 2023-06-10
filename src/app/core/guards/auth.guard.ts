1e6219246724ae5bef9f0b965634e0d06b788986
Gabriel windows <cydonian_07@hotmail.com>
2023-06-10 08:30:41 -0500

    migracios del core a angular 16

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,13 +1,5 @@
 import { Injectable } from "@angular/core";
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  CanLoad,
-  Route,
-  RouterStateSnapshot,
-  UrlSegment,
-  UrlTree,
-} from "@angular/router";
+import { ActivatedRouteSnapshot, Route, RouterStateSnapshot, UrlSegment, UrlTree } from "@angular/router";
 import { Observable, map } from "rxjs";
 import { Router } from "@angular/router";
 import { AuthService } from "../services/auth/auth.service";
@@ -15,7 +7,7 @@ import { AuthService } from "../services/auth/auth.service";
 @Injectable({
   providedIn: "root",
 })
-export class AuthGuard implements CanActivate, CanLoad {
+export class AuthGuard  {
   constructor(private router: Router, private authService: AuthService) {}
 
   redirec(flag: boolean) {
